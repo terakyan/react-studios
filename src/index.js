0bd4dc3bd76aa04f4ae0871518fc687824e66c6b
sahakavatar <hakobyan.sahak88@gmail.com>
2017-10-27 16:17:38 +0400

    up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './Css/Styles.css';
 import './App.css';
 import Preview from './Studio/Preview';
 import Settings from './Studio/Settings';
